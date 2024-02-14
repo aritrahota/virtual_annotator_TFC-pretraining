b38abcd1486fefe461b7a74a18ca0636136fca3d
aritrahota <aritra828207@gmail.com>
2024-02-15 00:43:22 +0530

    sh file added for one line execution

---
+++ code/TFC/exp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+dataset=$1
+
+python main.py --training_mode pre_train --pretrain_dataset $dataset --target_dataset Gesture
+# python main.py --training_mode exp_test_emb --pretrain_dataset $dataset --target_dataset $dataset
