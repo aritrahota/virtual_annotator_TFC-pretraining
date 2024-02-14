b31e0da1e688d201806b3243cdb431c384439104
aritrahota <aritra828207@gmail.com>
2024-02-15 00:52:24 +0530

    exp.sh file modified

--- code/TFC/exp.sh
+++ code/TFC/exp.sh
@@ -2,5 +2,5 @@
 
 dataset=$1
 
-python main.py --training_mode pre_train --pretrain_dataset $dataset --target_dataset Gesture
+python main.py --training_mode pre_train --pretrain_dataset $dataset --target_dataset Epilepsy
 # python main.py --training_mode exp_test_emb --pretrain_dataset $dataset --target_dataset $dataset
